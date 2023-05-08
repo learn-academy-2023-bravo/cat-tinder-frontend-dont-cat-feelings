3aa3a441e21f838fd284d33681ee591506d21e4b
Valeria <valeriacasillas123@yahoo.com>
2023-05-08 11:49:33 -0700

    update functionality

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ const App = () => {
   const createCat =(createdCat) => {
     console.log("Created Cat:", createCat)
   }
- 
+ const updateCat = (cat,id) =>{
+  console.log("cat:", cat)
+  console.log("id:", id)
+ }
   return (
     <>
       <Header />
@@ -28,7 +31,7 @@ const App = () => {
         <Route path="/catindex" element={<CatIndex cats = { cats }/>}/>
         <Route path="/catshow/:id" element={<CatShow cats = { cats }/>}/>
         <Route path="/catnew" element={<CatNew createCat={createCat}/>}/>
-        <Route path="/catedit" element={<CatEdit/>}/>
+        <Route path="/catedit/:id" element={<CatEdit cats={cats} updateCat={updateCat}/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     
--- src/pages/CatEdit.js
+++ src/pages/CatEdit.js
@@ -1,8 +1,53 @@
-const CatEdit = () => {
-    return(
-        <>
-        <h3>Edit the cat</h3></>
-    )
+import { useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { Form, formGroup, Label, Input, Button} from "react-router-dom"
+import { FormGroup } from "reactstrap"
+            
+const CatEdit = ({ cats, updateCat }) => {
+        const { id } = useParams()
+        let currentCat = cats?.find((cat) => cat.id === +id)
+    }
+    const [editCat, setEditCat] = useState({
+        name: currentCat.name,
+        age: currentCat.age,
+        enjoyments: currentCat.enjoyments,
+        image: currentCat.image,
+        ideal_date: currentCat.ideal_date
+    })
+    const handleChange = (e) => {
+        setEditCat({ ...editCat, [e.target.name]: e.target.value })
+    }
+    const handleSubmit =()=>{
+        updateCat(editCat,currentCat.id)
+    }
+
+return(
+    <>
+    <Form>
+<FormGroup>
+    <Label for="name">Name</Label>
+    <Input type="text" name="name" />
+</FormGroup>
+<FormGroup>
+    <Label for="age">Age</Label>
+    <Input type="number" name="age" />
+</FormGroup>
+<FormGroup>
+    <Label for="enjoyments">Enjoys</Label>
+    <Input type="text" name="enjoyments" />
+</FormGroup>
+<FormGroup>
+    <Label for="image">Image URL</Label>
+    <Input type="text" name="image" />
+</FormGroup>
+<FormGroup>
+    <Label for="ideal date">Ideal Date </Label>
+    <Input type="text" name="ideal_Date"/>
+</FormGroup>
+    </Form>
+        
+    </>
+)
 }
 
 export default CatEdit
